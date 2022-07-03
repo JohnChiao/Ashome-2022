b98776db50be3395622d9a13bcf3623d946415be
JohnChiao <johnchiao@outlook.com>
2022-07-03 10:44:34 +0800

    Ashome database<TEST>
    相关工作项: #10

--- 2022/Applet/db.py
+++ 2022/Applet/db.py
@@ -1,6 +1,12 @@
-class my_class(object):
-    pass
+import sqlite3
 
+class Database(object):
+	def __init__(self,name = "root.db"):
+		self.connect = sqlite3.connect(name)
+		self.cursor = self.connect.cursor()
 
-
-
+class Table(Database):
+	def __init__(self,name = "Table",*data):
+			strdata = ", ".join(data)
+			code = "CREATE TABLE "+name+"("+strdata+")"
+			self.cursor.execute(code)
