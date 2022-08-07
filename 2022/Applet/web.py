55facec808c2a7c3057e302210743fbcd1a2b4df
JohnChiao <johnchiao@outlook.com>
2022-08-07 13:06:39 +0800

    Ashome log

--- 2022/Applet/web.py
+++ 2022/Applet/web.py
@@ -1,15 +1,16 @@
 import webbrowser
 import requests
 
-def search(kw,se = 'https://cn.bing.com/search?q=%s'):
-    url = se % kw
+def search():
+    kw = input('Search')
+    url = 'https://www.google.com/search?q=' + kw
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0',}
     response = requests.get(url=url, headers=headers)
-    fileName = 'Cache.html'
+    fileName = '__pycache__/webcache.html'
     with open(fileName, 'w', encoding='utf-8') as fp:
         fp.write(response.text)
     webbrowser.open(fileName)
-    return 0;
+
 
 def web(url,*arg):
     '/'.join(url,*arg)
--- 2022/__init__.py
+++ 2022/__init__.py
@@ -3,29 +3,39 @@ import platform
 import setup
 VERSION = "2022.10.5"
 code = ""
+cachefile = open("__pycache__/log.txt",mode='w')
+cachefile.close()
 
 
 def main():
 	code = "Undefined"
+	cachefile = open("__pycache__/log.txt",mode='a+')
 	while code:
 		code = input(User.activaty.name+"@"+os.getcwd()+">")
 		try:
 			exec(User.activaty.history if code == "-h" else code)
 			User.activaty.history = User.activaty.history if code == "-h" else code
-			print("Run:\r\n\tCode:\r\n\t\t",User.activaty.history,"\r\n")
+			cachefile.write("Run:\r\n\tCode:\r\n\t\t",User.activaty.history,"\r\n")
 		except KeyboardInterrupt:
 			shutdown()
 		except NameError as err:
 			print("NameError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
+			cachefile.write("NameError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
 		except TypeError as err:
 			print("TypeError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
+			cachefile.write("TypeError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
 		except IOError as err:
 			print("IOError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
+			cachefile.write("IOError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
 		except KeyError as err:
 			print("KeyError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
+			cachefile.write("KeyError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
 		except ValueError as err:
 			print("ValueError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
-
+			cachefile.write("ValueError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
+		except AttributeError as err:
+			print("AttributeError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
+			cachefile.write("AttributeError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
 
 
 def launch(module,code):
@@ -42,6 +52,8 @@ def launch(module,code):
 		print("IOError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
 	except KeyError as err:
 		print("KeyError:\r\n\tCode:\r\n\t\t",code,"\r\n\tDatail:\r\n\t\t",err,"\r\n")
+	finally:
+
 
 
 def shutdown():
