2270b3d8af736a8df3d8fe41a3cfd0aa77aaab28
JohnChiao <johnchiao@outlook.com>
2022-09-18 19:00:28 +0800

    mdf flask

--- 2022/Flask/server.py
+++ 2022/Flask/server.py
@@ -3,9 +3,9 @@ import flask
 app = flask.Flask(__name__)
 
 """
-    A Flask server
-    New HTML templates in <./templates>
-    New static file in <./statics>
+	A Flask server
+	New HTML templates in <./templates>
+	New static file in <./statics>
 """
 
 
@@ -13,9 +13,9 @@ app = flask.Flask(__name__)
 def mainpage():
 	return flask.render_template("index.html")
 
-
+		
 def main():
-    try:
-	    app.run()
-    except as err:
-        print("Error:",err)
+	try:
+		app.run()
+	except:
+		pass
