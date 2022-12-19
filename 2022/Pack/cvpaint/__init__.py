385c69fd5d27f52ddb8500c98c67f127f3950532
JohnChiao <johnchiao@outlook.com>
2022-12-19 21:40:28 +0800

    CVPaint V3.0.0: - view only mode

--- 2022/Pack/cvpaint/__init__.py
+++ 2022/Pack/cvpaint/__init__.py
@@ -3,11 +3,10 @@ from turtle import *
 
 class Canva(object):
 	def __init__(self, start = [0,0], pendown = True):
+		setup(800, 800, 0, 0)
 		hideturtle()
 		self.pen = Pen()
 		self.dpen = self.pen
-		self.pen.penup()
-		self.pen.goto(*start)
 		if pendown:
 			self.pen.pendown()
 		print("Created canva")
@@ -20,6 +19,7 @@ class Canva(object):
 		pen = Pen()
 		print("Changed pen to",pen if pen else self.pen)
 		self.pen = pen if pen else self.pen
+		return pen
 
 	def d(self, startx = 0, starty = 0, endx = 0, endy = 0):
 		self.pen.penup()
@@ -47,6 +47,33 @@ class Canva(object):
 
 	def c(self, r = 0, posx = 0, posy = 0):
 		self.pen.penup()
-		self.pen.goto(posx + r, posy)
+		self.pen.goto(posx, posy - r)
 		self.pen.pendown()
 		self.pen.circle(r,360)
+
+	def g(self, posx = 0, posy = 0):
+		self.pen.goto(posx, posy)
+
+	def f(self):
+		input("Press Enter to finish")
+		print("Canva", self, "is closed")
+		self.pen.hideturtle()
+		del self
+
+	def CLOSE(self):
+		del self
+		return 1
+
+	def v(self):
+		print("VIEW ONLY MODE")
+		try:
+			while True:
+				pass
+		except KeyboardInterrupt:
+			self.pen.reset()
+			self.pen.hideturtle()
+			return 0
+
+		
+
+dc = Canva()
