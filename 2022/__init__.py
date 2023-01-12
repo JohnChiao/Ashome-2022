a6ba5303b05e87c07e7b762a234391a38404036c
JohnChiao <johnchiao@outlook.com>
2023-01-12 12:48:30 +0800

    det user control

--- 2022/__init__.py
+++ 2022/__init__.py
@@ -15,7 +15,7 @@ code = ""
 _prompt = {"nt" : " > ", "posix" : " $ "}
 cter = False
 username = getpass.getuser()
-user = User(username)
+user = User(username, "root")
 print("Python V"+platform.python_version())
 # Global Define End
 # Login
@@ -69,31 +69,31 @@ def main():
 
 			except NameError as err:
 				print("\n\r", "NameError:\n\tCode:\n\t\t",
-				      code, "\n\tDatail:\n\t\t", err, "\n")
+                                    code, "\n\tDatail:\n\t\t", err, "\n")
 
 			except TypeError as err:
 				print("\n\r", "TypeError:\n\tCode:\n\t\t",
-				      code, "\n\tDatail:\n\t\t", err, "\n")
+                                    code, "\n\tDatail:\n\t\t", err, "\n")
 
 			except IOError as err:
 				print("\n\r", "IOError:\n\tCode:\n\t\t",
-				      code, "\n\tDatail:\n\t\t", err, "\n")
+                                    code, "\n\tDatail:\n\t\t", err, "\n")
 				
 			except KeyError as err:
 				print("\n\r", "KeyError:\n\tCode:\n\t\t",
-				      code, "\n\tDatail:\n\t\t", err, "\n")
+                                    code, "\n\tDatail:\n\t\t", err, "\n")
 
 			except ValueError as err:
 				print("\n\r", "ValueError:\n\tCode:\n\t\t",
-				      code, "\n\tDatail:\n\t\t", err, "\n")
+                                    code, "\n\tDatail:\n\t\t", err, "\n")
 
 			except AttributeError as err:
 				print("\n\r", "AttributeError:\n\tCode:\n\t\t",
-				      code, "\n\tDatail:\n\t\t", err, "\n")
+                                    code, "\n\tDatail:\n\t\t", err, "\n")
 
 			except IndexError as err:
 				print("\n\r", "IndexError:\n\tCode:\n\t\t",
-				      code, "\n\tDatail:\n\t\t", err, "\n")
+                                    code, "\n\tDatail:\n\t\t", err, "\n")
 			
 			except:
 				print("\n\r", "Error:\n\tCode:\n\t\t", code)
@@ -117,16 +117,24 @@ def start():
 		if easygui.ccbox("Welcome!","Start",["[L]ogin or Sign up","[Q]uit"]):
 			ulogin = easygui.choicebox("Login","Login",USERS+["Sign Up..."])
 			if ulogin == "Sign Up...":
-				t = easygui.enterbox("Input User Name:","Sign Up")
-				User(name = t)
-				main()
+				t = easygui.multpasswordbox("Sign up:", "Sign Up", [
+				                            "Username", "Password"])
+				if not easygui.ynbox("Sign up "+t[0]+"?", "Sign Up"):
+					t = None
+				if t[0]:
+					User(*t)
+					main()
 			elif ulogin:
-				User(name = ulogin)
-				main()
+				try:
+					passwd = easygui.passwordbox("Input password:", "Login")
+					User(name=ulogin, passwd=passwd)
+					main()
+				except ConnectionError as err:
+					easygui.msgbox(err, "Error", "Cencel")
 			else:
 				continue
 		else:
-			shutdown()
+			shutdown
 
 
 if __name__ == "__main__":
