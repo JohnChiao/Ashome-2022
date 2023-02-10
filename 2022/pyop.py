53465e4cf56872d81db3240edc03c3ad5424ecef
JohnChiao <johnchiao@outlook.com>
2023-02-10 20:57:03 +0800

    erase pyop

--- 2022/pyop.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def python(name = "", options = "", func = ""):
-	os.system("python "+options+" "+name+" "+func)
-
-
-def python3(name = "", options = "", func = ""):
-	os.system("python3 "+options+" "+name+" "+func)
